44930382ad5e78bf5febcbf1c78df550daf472a5
Yassine El Bouziady <elbouziady.yassine@gmail.com>
2023-01-13 19:06:54 +0100

    save

--- apps/docs/.storybook/main.js
+++ apps/docs/.storybook/main.js
@@ -1,25 +1,15 @@
 module.exports = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
   framework: {
     name: "@storybook/nextjs",
     options: {},
   },
   docs: {
     autodocs: "tag",
-  },
-  refs: {
-    web: {
-      title: "Web App",
-      url: "http://localhost:6007",
-    },
-    ui: {
-      title: "Design System",
-      url: "http://localhost:6008",
-    },
-    shop: {
-      title: "Shop",
-      url: "https://634983abe48e036f18c3faeb-iyvehnzayu.chromatic.com",
-    },
   },
 };
--- apps/web/src/components/Welcome.tsx
+++ apps/web/src/components/Welcome.tsx
@@ -1,5 +1,4 @@
 import cx from "./Welcome.module.css";
-
 export function Welcome() {
   return <div className={cx["welcome"]}>Welcome To website</div>;
 }
