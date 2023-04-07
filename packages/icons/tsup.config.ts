2886d72c63e2c25483eed2cbb1c516c2bc55ff7a
Yassine El Bouziady <elbouziady.yassine@gmail.com>
2023-04-07 17:24:34 +0000

    feat: add cjs support for icons library

--- packages/icons/tsup.config.ts
+++ packages/icons/tsup.config.ts
@@ -1,10 +1,28 @@
-import { defineConfig } from 'tsup';
+import { defineConfig, Options } from 'tsup';
 
-export default defineConfig({
+const baseConfig: Options = {
   entry: ['src'],
   clean: true,
-  format: ['esm'],
   target: 'esnext',
-  minify: true,
+  // will be minified at the consuming app
+  minify: false,
   external: ['react'],
-});
+  splitting: false,
+  outExtension() {
+    return {
+      js: `.js`,
+    };
+  },
+};
+export default defineConfig([
+  {
+    ...baseConfig,
+    format: ['esm'],
+    outDir: 'dist/esm',
+  },
+  {
+    ...baseConfig,
+    format: ['cjs'],
+    outDir: 'dist/cjs',
+  },
+]);
